224524d6462d79c089388e69afa8bf24c031cab3
Attila Szabo <attila.szabo@logmein.com>
2015-01-21 16:56:36 -0800

    FIX: Can't mass-assign protected attributes: encrypted_password, password_salt

--- lib/devise_security_extension/models/old_password.rb
+++ lib/devise_security_extension/models/old_password.rb
@@ -1,4 +1,5 @@
 require 'active_record'
 class OldPassword < ActiveRecord::Base
   belongs_to :password_archivable, :polymorphic => true
+  attr_accessible :created_at, :encrypted_password, :password_archivable_id, :password_archivable_type, :password_salt
 end
