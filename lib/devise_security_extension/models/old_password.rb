cd87efed42239776997078ded3bf57f8776659d5
Attila Szabo <attila.szabo@logmein.com>
2015-01-23 18:11:33 -0800

    fix: keep only the required field

--- lib/devise_security_extension/models/old_password.rb
+++ lib/devise_security_extension/models/old_password.rb
@@ -1,5 +1,5 @@
 require 'active_record'
 class OldPassword < ActiveRecord::Base
   belongs_to :password_archivable, :polymorphic => true
-  attr_accessible :created_at, :encrypted_password, :password_archivable_id, :password_archivable_type, :password_salt
+  attr_accessible :encrypted_password, :password_salt
 end
